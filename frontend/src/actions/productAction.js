f264fffd2ccfd2c6a546ec6f5da448b00089effd
ImamAlfarizi <imam.alfarizi.777@gmail.com>
2021-07-13 19:41:28 +0700

    implement redux to get detail product from api

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -1,20 +1,49 @@
 import Axios from "axios";
-import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL } from "../constants/productConstants";
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
+} from "../constants/productConstants";
 
 export const listProducts = () => async (dispatch) => {
   dispatch({
-    type: PRODUCT_LIST_REQUEST
+    type: PRODUCT_LIST_REQUEST,
   });
   try {
     const { data } = await Axios.get("/api/products");
     dispatch({
       type: PRODUCT_LIST_SUCCESS,
-      payload: data
-    })
+      payload: data,
+    });
   } catch (err) {
     dispatch({
       type: PRODUCT_LIST_FAIL,
-      payload: err.message
+      payload: err.message,
     });
   }
-}
+};
+
+export const detailsProduct = (productId) => async (dispatch) => {
+  dispatch({
+    type: PRODUCT_DETAILS_REQUEST,
+    payload: productId,
+  });
+  try {
+    const { data } = await Axios.get(`/api/products/${productId}`);
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message,
+    });
+  }
+};
