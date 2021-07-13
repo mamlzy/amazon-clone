8594f888ab330dc1e5bb4f17303c714121415865
ImamAlfarizi <imam.alfarizi.777@gmail.com>
2021-07-13 09:49:06 +0700

    init node backend

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import data from './data.js';
+
+const app = express();
+
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
+
+app.get('/', (req, res) => {
+  res.send('Server is ready');
+});
+
+app.listen(5000, () => {
+  console.log(`Serve at http://localhost:5000`);
+})
