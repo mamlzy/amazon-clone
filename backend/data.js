77ae475a337138c2e7ea0d6d6039d27227bd0e43
ImamAlfarizi <imam.alfarizi.777@gmail.com>
2021-07-14 10:34:35 +0700

    seed table products, now products get from mongodb

--- backend/data.js
+++ backend/data.js
@@ -17,7 +17,6 @@ const data = {
   ],
   products: [
     {
-      _id: '1',
       name: 'Nike Slim Shirt',
       category: 'Shirts',
       image: '/images/p1.jpg',
@@ -26,10 +25,9 @@ const data = {
       brand: 'Nike',
       rating: 3.5,
       numReviews: 11,
-      descrption: 'high quality product',
+      description: 'high quality product',
     },
     {
-      _id: '2',
       name: 'Adidas Fit Shirt',
       category: 'Shirts',
       image: '/images/p2.jpg',
@@ -38,10 +36,9 @@ const data = {
       brand: 'Nike',
       rating: 2.5,
       numReviews: 11,
-      descrption: 'high quality product',
+      description: 'high quality product',
     },
     {
-      _id: '3',
       name: 'Lacoste Free Shirt',
       category: 'Shirts',
       image: '/images/p3.jpg',
@@ -50,10 +47,9 @@ const data = {
       brand: 'Lacoste',
       rating: 4.8,
       numReviews: 17,
-      descrption: 'high quality product',
+      description: 'high quality product',
     },
     {
-      _id: '4',
       name: 'Nike Slim Pant',
       category: 'Pants',
       image: '/images/p4.jpg',
@@ -62,10 +58,9 @@ const data = {
       brand: 'Nike',
       rating: 5,
       numReviews: 14,
-      descrption: 'high quality product',
+      description: 'high quality product',
     },
     {
-      _id: '5',
       name: 'Puma Slim Shirt',
       category: 'Shirts',
       image: '/images/p5.jpg',
@@ -74,10 +69,9 @@ const data = {
       brand: 'Puma',
       rating: 4.5,
       numReviews: 11,
-      descrption: 'high quality product',
+      description: 'high quality product',
     },
     {
-      _id: '6',
       name: 'Adidas Fit Pant',
       category: 'Pants',
       image: '/images/p6.jpg',
@@ -86,7 +80,7 @@ const data = {
       brand: 'Adidas',
       rating: 4.5,
       numReviews: 15,
-      descrption: 'high quality product',
+      description: 'high quality product',
     },
   ],
 };
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose';
-import data from './data.js';
+import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 
 const app = express();
@@ -11,21 +11,7 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/amazon-cl
 })
 
 app.use('/api/users', userRouter);
-
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find(x => x._id === req.params.id);
-  if(product) {
-    res.send(product);
-  } else {
-    res.status(404).send({
-      message: 'Product Not Found',
-    });
-  }
-});
+app.use('/api/products', productRouter);
 
 app.use((err, req, res, next) => {
   res.status(500).send({
